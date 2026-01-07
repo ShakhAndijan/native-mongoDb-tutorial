9783f54de39afeaa517fef9ac273b0792cc064d9
ShakhBek <shakhandijan@gmail.com>
2026-01-07 10:30:01 +0500

    0701 10.29

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -1,23 +1,32 @@
 import tracker from '../api/tracker';
 import createDataContext from './createDataContext';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const authReducer = (state, action) => {
   switch (action.type) {
+    case 'add_error':
+      return { ...state, errorMessage: action.payload };
+    case 'signup':
+      return { errorMessage: '', token: action.payload };
     default:
       return state;
   }
 };
 
-const signup = (dispatch) => {
-  return async ({ email, password }) => {
+const signup =
+  (dispatch) =>
+  async ({ email, password }) => {
     try {
       const response = await tracker.post('/signup', { email, password });
-      console.log(response.data);
+      await AsyncStorage.setItem('token', response.data.token);
+      dispatch({ type: 'signup', payload: response.data.token });
     } catch (err) {
-      console.log(err.message);
+      dispatch({
+        type: 'add_error',
+        payload: 'Something went wrong with sign up',
+      });
     }
   };
-};
 
 const signin = (dispatch) => {
   return ({ email, password }) => {};
@@ -31,5 +40,5 @@ export const { Context: AuthContext, Provider: AuthProvider } =
   createDataContext(
     authReducer,
     { signin, signout, signup },
-    { isSignedIn: false }
+    { token: null, errorMessage: '' }
   );
